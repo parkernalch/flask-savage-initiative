969f545e9a81e91831c227f60690616f028b6ac8
Parker Nalchajian <pnalchajian@outlyertech.com>
2019-04-25 22:34:24 -0700

    got rid of unused code, fixed the loader on modal call/save

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -1,119 +1,24 @@
 $(document).ready(function() {
-    // console.log('page alert from home.js')
-    //$('body').load('/party');
-});
-
-//$("#partyMemberForm").submit(function(){
-$("#AddPartyMember").on("click", function(){
-    //alert("handler for submit called");
-    let charName = $("#characterNameInput").val();
-    let tactLv = $("input[type=radio][name=tacticianOptions]:checked").val();
-    let lvlHead = $("input[type=radio][name=levelHeadedOptions]:checked").val();
-    let quick = $("input[type=radio][name=quickHesitantOptions][id=quickEdge]:checked").val() || 0;
-    let hesitant = $("input[type=radio][name=quickHesitantOptions][id=hesitantHindrance]:checked").val() || 0;
-    
-    let sessionID = $("#partyMemberForm").attr('data-id');
-
-    let json_party_member = {
-        'name': charName,
-        'tactician': parseInt(tactLv),
-        'level_headed': parseInt(lvlHead),
-        'quick': parseInt(quick),
-        'hesitant': parseInt(hesitant)
-    };
-
+    console.log('page loaded')
     $.ajax({
-        type: 'POST',
-        url: '/' + sessionID + '/party/add',
-        data: JSON.stringify(json_party_member),
-        dataType: 'json',
-        contentType: 'application/json',
-        success: function() {
-            $('body').load('/party')
+        type: 'GET',
+        url: '/party',
+        dataType: "html",
+        success: function(response) {
+            $('#page-content').html(response);
         },
         error: function(e) {
-            alert('POST FAILED');
+            alert('No Party Received');
         }
     });
-
-})
+});
 
 $("#startInitiative").on("click", function(){
     alert("starting initative...")
 });
 
-$(".edit-character-button").on('click', function() {
-    let charname = $(this).attr('data-charname');
-    $("#character-modal").attr('data-oldname', charname);
-    charname = charname.replace(" ", "-").toLowerCase();
-    let character = null;
-
-    let sessionID = $('#party').attr('data-sessionid');
-
-    $.ajax({
-        type: 'GET',
-        url: '/' + sessionID + '/party/' + charname,
-        success: function(response){
-            character = response;
-            // console.log(character);
-            $("#modal-charname-input").val(character.name);
-            
-            for(var i=0; i < 3; i++){
-                let query = "#levelHeaded" + i.toString();
-                let query2 = ".lvlheadlabel" + i.toString();
-                if(i === character.level_headed){
-                    $(query).prop('checked', true);
-                    $(query2).addClass("active");
-                } else {
-                    $(query).prop('checked', false);
-                    $(query2).removeClass("active");
-                }
-            }
-            
-            for(var j=0; j<3; j++){
-                let query = "#tactician" + j.toString();
-                let query2 = ".tactlabel" + j.toString();
-                if(j === character.tactician){
-                    $(query).prop('checked', true);
-                    $(query2).addClass("active");
-                } else {
-                    $(query).prop('checked', false);
-                    $(query2).removeClass("active");
-                }
-            }
-           
-            if(character.quick > 0){
-                $("#quickEdge").prop('checked', true);
-                $(".quicklabel").addClass("active");
-                $("#hesitantHindrance").prop('checked', false);
-                $(".hesitantlabel").removeClass("active");
-                $("#noQuickHesitant").prop('checked', false);
-                $(".nonelabel").removeClass("active");
-            } else if (character.hesitant > 0) {
-                $("#quickEdge").prop('checked', false);
-                $(".quicklabel").removeClass("active");
-                $("#hesitantHindrance").prop('checked', true);
-                $(".hesitantlabel").addClass("active");
-                $("#noQuickHesitant").prop('checked', false);
-                $(".nonelabel").removeClass("active");
-            } else {
-                $("#quickEdge").prop('checked', false);
-                $(".quicklabel").removeClass("active");
-                $("#hesitantHindrance").prop('checked', false);
-                $(".hesitantlabel").removeClass("active");
-                $("#noQuickHesitant").prop('checked', true);
-                $(".nonelabel").addClass("active");
-            }
-        },
-        error: function(e){
-            alert("Character Failed to Load");
-        }
-    });
-
-    $("#character-modal").modal();
-});
-
 $("#submit-modal").on('click', function(){
+    console.log("Modal Submit called");
     let oldName = $("#character-modal").attr("data-oldname");
     if(oldName === ""){
         oldName = "new character"
@@ -139,17 +44,18 @@ $("#submit-modal").on('click', function(){
         type: 'POST',
         url: '/' + sessionID + '/party/' + oldName.replace(" ", "-").toLowerCase(),
         data: JSON.stringify(json_party_member),
-        dataType: 'json',
+        dataType: 'html',
         contentType: 'application/json',
-        success: function() {
-            $('body').load('/party');
+        success: function(response) {
+            // console.log(response);
+            $("#page-content").html(response);
+            $("#character-modal").modal('toggle');
         },
         error: function(e) {
             alert('POST FAILED');
         }
 
     });
-
 });
 
 $("#start-initiative-btn").on("click", function() {
@@ -158,26 +64,57 @@ $("#start-initiative-btn").on("click", function() {
 
 $("#new-character-navbutton").on("click", function() {
     $("#modal-charname-input").val("");
-    $("#levelHeaded0").prop('checked', true)
-    $(".lvlheadlabel0").addClass("active")
-    $("#levelHeaded1").prop('checked', false)
-    $(".lvlheadlabel1").removeClass("active")
-    $("#levelHeaded2").prop('checked', false)
-    $(".lvlheadlabel2").removeClass("active")
+    $("#levelHeaded0").prop('checked', true);
+    $(".lvlheadlabel0").addClass("active");
+    $("#levelHeaded1").prop('checked', false);
+    $(".lvlheadlabel1").removeClass("active");
+    $("#levelHeaded2").prop('checked', false);
+    $(".lvlheadlabel2").removeClass("active");
 
-    $("#tactician0").prop('checked', true)
-    $(".tactlabel0").addClass("active")
-    $("#tactician1").prop('checked', false)
-    $(".tactlabel1").removeClass("active")
-    $("#tactician2").prop('checked', false)
-    $(".tactlabel2").removeClass("active")
+    $("#tactician0").prop('checked', true);
+    $(".tactlabel0").addClass("active");
+    $("#tactician1").prop('checked', false);
+    $(".tactlabel1").removeClass("active");
+    $("#tactician2").prop('checked', false);
+    $(".tactlabel2").removeClass("active");
 
-    $("#noQuickHesitant").prop('checked', true)
-    $(".nonelabel").addClass("active")
-    $("#quickEdge").prop('checked', false)
-    $(".quicklabel").removeClass("active")
-    $("#hesitantHindrance").prop('checked', false)
-    $(".hesitantlabel").removeClass("active")
-     
+    $("#noQuickHesitant").prop('checked', true);
+    $(".nonelabel").addClass("active");
+    $("#quickEdge").prop('checked', false);
+    $(".quicklabel").removeClass("active");
+    $("#hesitantHindrance").prop('checked', false);
+    $(".hesitantlabel").removeClass("active");
+    
+    console.log($("#character-modal"));
     $("#character-modal").modal();
+});
+
+$(".join-table").on('click', function(){
+    // alert("join table button pushed");
+    $.ajax({
+        url: '/tables',
+        type: 'GET',
+        dataType: 'html',
+        success: function(response) {
+            $("#page-content").html(response);
+        },
+        error: function() {
+            alert('Join Table GET failed');
+        }
+    });
+});
+
+$(".create-table").on('click', function() {
+    // alert("create table button pushed");
+    $.ajax({
+        url: '/tables',
+        type: 'GET',
+        dataType: 'html',
+        success: function(response) {
+            $("#page-content").html(response);
+        },
+        error: function() {
+            alert('Join Table GET failed');
+        }
+    });
 });
